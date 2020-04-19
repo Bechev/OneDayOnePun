009c509eaad8dd2df43b16fe059fd750bc9e45b3
bct <bertrand.chevalier@gmail.com>
2020-04-19 18:23:28 -0400

    fixing bugs

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key: 'hIFr1zrwqCLTGWKrJCQ5M9B62',  
-    consumer_secret: 'nMIlRorag1qr0d1s4OzwJkUMd3BBSQKQYIpTWf6BIw3Rox65hK',
-    access_token: '1247957035200786433-Vn47ChOmUZ8ZzxKUHlv57Iup7fk0tN',  
-    access_token_secret: 'OhMSwbaI1qwltCRS4smRG7hONdua5OlYBdfrqccHZcgvH'
+    consumer_key: 'HjMMC5fzTIh6ddqYQYyuvTdmr',
+    consumer_secret: 'tkWCUNFTCOowDwYxJaIHTxf8YSGp8GlfafxBW8f3YyhsIiDi5p',
+    access_token: '1247957035200786433-6HfPhclEz4DDC9eGbIfrK3rLMzGDaI',
+    access_token_secret: 'WRlkmSLFtE2AOrHGdLY0lv8H8hVnfUFxhAjZY2m0fx8no'
   }
 
